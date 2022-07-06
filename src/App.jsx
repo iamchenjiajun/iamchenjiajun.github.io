8472338af783038d1984092a0535eeda618db556
Chen Jia Jun <iamchenjiajun@gmail.com>
2022-07-06 20:26:37 +0800

    Add initial About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Nav from './components/Nav';
+import About from './components/About'
 
 function App() {
   return (
     <>
       <Header />
       <Nav />
+      <About />
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './about.css'
+
+function About() {
+  return (
+    <div className="container about__container" id='about'>
+        <article>
+            <h2>about me</h2>
+            <p>
+              I am Jia Jun, a final year Computer Engineering student, currently studying at the National Univeristy of Singapore (NUS).
+              My interests are in the area of Embedded Systems and Web Development. I like to write programs and work with electronics.
+            </p>
+        </article>
+    </div>
+  )
+}
+
+export default About
