595e8a4ccf86192908c72a12b284d4708835a597
Chen Jia Jun <iamchenjiajun@gmail.com>
2022-07-08 02:37:23 +0800

    Add drop shadows to header, footer, skills item

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -3,13 +3,15 @@ import './footer.css'
 
 function Footer() {
   return (
-    <footer className='outer__container footer__outer'>
-        <div className="container footer__container">
-            <h2>Jia Jun</h2>
-            <p>Made with create-react-app and react-icons.</p>
-            <a className='footer__source-code' href="https://github.com/iamchenjiajun/iamchenjiajun.github.io">Source code</a>
-        </div>
-    </footer>
+    <div className='footer__wrapper'>
+        <footer className='outer__container footer__outer'>
+            <div className="container footer__container">
+                <h2>Jia Jun</h2>
+                <p>Made with create-react-app and react-icons.</p>
+                <a className='footer__source-code' href="https://github.com/iamchenjiajun/iamchenjiajun.github.io">Source code</a>
+            </div>
+        </footer>
+    </div>
   )
 }
 
--- src/components/page/Header.jsx
+++ src/components/page/Header.jsx
@@ -4,21 +4,23 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 function Header() {
   return (
-    <header className='outer__container header__outer'>
-      <div className="container header__container">
-        <img className='avatar' src="https://github.com/iamchenjiajun.png" alt="Me" />
-        <div className="header__container__about">
-          <div className="header__container__about__name">
-            <h4>hello, i'm</h4>
-            <h2>Chen Jia Jun</h2>
-          </div>
-          <div className="header__container__about__links">
-            <a href="https://www.github.com/iamchenjiajun"><FaGithub/></a>
-            <a href="https://www.linkedin.com/in/iamchenjiajun"><FaLinkedinIn/></a>
+    <div className='header__wrapper'>
+      <header className='outer__container header__outer'>
+        <div className="container header__container">
+          <img className='avatar' src="https://github.com/iamchenjiajun.png" alt="Me" />
+          <div className="header__container__about">
+            <div className="header__container__about__name">
+              <h4>hello, i'm</h4>
+              <h2>Chen Jia Jun</h2>
+            </div>
+            <div className="header__container__about__links">
+              <a href="https://www.github.com/iamchenjiajun"><FaGithub/></a>
+              <a href="https://www.linkedin.com/in/iamchenjiajun"><FaLinkedinIn/></a>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   )
 }
 
