16cb12cd2c3ed54660ebd50476cbc903b56ee07d
Chen Jia Jun <iamchenjiajun@gmail.com>
2022-07-18 21:15:31 +0800

    Update indentation in project_data.jsx

--- src/components/data/projects/project_data.jsx
+++ src/components/data/projects/project_data.jsx
@@ -1,28 +1,30 @@
 const project_data = [
-    {
-      title: "Neural Network Accelerator on FPGA",
-      description: "This project contains a neural network accelerator implemented on FPGA with Verilog (HDL) and C++ (HLS).",
-      img: 'https://github.com/iamchenjiajun.png',
-      category: "embedded",
-      markup: <>
-        <h2>Neural Network Accelerator on FPGA</h2>
-        <p>category: <em>embedded</em></p>
-        <p>This project contains a neural network accelerator implemented on FPGA with Verilog (HDL) and C++ (HLS).</p>
-      </>,
-    },
-    {
-      title: "Minesweeper Duels",
-      description: "24h hackathon project, 2 player real-time multiplayer built with socket.io and expressJS. Minesweeper Duels is a 2 player game where each player takes turns to reveal a space in the Minesweeper board. However, each player only has a limited amount of time, where it ticks down during your turn. Clicking on a mine deducts additional time.",
-      img: 'https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/325/940/datas/gallery.jpg',
-      source: "https://github.com/iamchenjiajun/minesweeper-duels",
-      demo: "https://minesweeper-royale.herokuapp.com/",
-      category: "web",
-      markup: <>
-        <h2>Minesweeper Duels</h2>
-        <p>category: <em>web development</em></p>
-        <p>24h hackathon project, 2 player real-time multiplayer built with socket.io and expressJS. Minesweeper Duels is a 2 player game where each player takes turns to reveal a space in the Minesweeper board. However, each player only has a limited amount of time, where it ticks down during your turn. Clicking on a mine deducts additional time.</p>
-      </>,
-    },
+	{
+		title: "Neural Network Accelerator on FPGA",
+		description: "This project contains a neural network accelerator implemented on FPGA with Verilog (HDL) and C++ (HLS).",
+		img: 'https://github.com/iamchenjiajun.png',
+		category: "embedded",
+		markup:
+		<>
+			<h2>Neural Network Accelerator on FPGA</h2>
+			<p>category: <em>embedded</em></p>
+			<p>This project contains a neural network accelerator implemented on FPGA with Verilog (HDL) and C++ (HLS).</p>
+		</>,
+	},
+	{
+		title: "Minesweeper Duels",
+		description: "24h hackathon project, 2 player real-time multiplayer built with socket.io and expressJS. Minesweeper Duels is a 2 player game where each player takes turns to reveal a space in the Minesweeper board. However, each player only has a limited amount of time, where it ticks down during your turn. Clicking on a mine deducts additional time.",
+		img: 'https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/325/940/datas/gallery.jpg',
+		source: "https://github.com/iamchenjiajun/minesweeper-duels",
+		demo: "https://minesweeper-royale.herokuapp.com/",
+		category: "web",
+		markup:
+		<>
+			<h2>Minesweeper Duels</h2>
+			<p>category: <em>web development</em></p>
+			<p>24h hackathon project, 2 player real-time multiplayer built with socket.io and expressJS. Minesweeper Duels is a 2 player game where each player takes turns to reveal a space in the Minesweeper board. However, each player only has a limited amount of time, where it ticks down during your turn. Clicking on a mine deducts additional time.</p>
+		</>,
+	},
 ]
 
 export default project_data
