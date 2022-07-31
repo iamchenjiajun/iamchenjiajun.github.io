dea7ba60424fd0a147cf2c67cbab42a0ec4fbe57
Chen Jia Jun <iamchenjiajun@gmail.com>
2022-08-01 04:51:12 +0800

    Update footer content

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -7,7 +7,8 @@ function Footer() {
         <footer className='outer__container footer__outer'>
             <div className="container footer__container">
                 <h2>Jia Jun</h2>
-                <p>Made with create-react-app and react-icons.</p>
+                <p>Made with React using create-react-app and react-icons.</p>
+                <p>Deployed with gh-pages.</p>
                 <a className='footer__source-code' href="https://github.com/iamchenjiajun/iamchenjiajun.github.io">Source code</a>
             </div>
         </footer>
